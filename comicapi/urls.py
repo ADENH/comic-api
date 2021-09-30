10b8c9e8d3cd4327690a0d5efadc9a5cc11df8ca
adenurhidayat <adenurhidayat101294@gmail.com>
2021-09-30 08:48:14 +0700

    modify : remove trailing slash

--- comicapi/urls.py
+++ comicapi/urls.py
@@ -9,10 +9,10 @@ router.register(r'comics', views.ComicViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('search-comic/', views.search_comic, name="search_comic"),
-    path('popular-comic/',views.popular_comic ,name="popular_comic"),
-    path('detail-comic/',views.detail_comic ,name="detail_comic"),
-    path('read-comic/', views.read_comic, name="read_comic"),
-    path('list-comic/', views.list_comic, name="list_comic"),
+    path('search-comic', views.search_comic, name="search_comic"),
+    path('popular-comic',views.popular_comic ,name="popular_comic"),
+    path('detail-comic',views.detail_comic ,name="detail_comic"),
+    path('read-comic', views.read_comic, name="read_comic"),
+    path('list-comic', views.list_comic, name="list_comic"),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     ]
